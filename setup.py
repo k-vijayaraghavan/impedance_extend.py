8281a618f81d7728f164dc92504db8c7f327b327
Krishna Vijayaraghavan <prof.krishna.v@gmail.com>
2026-05-11 15:57:33 -0700

    Preparation for version 1 and fix for flake

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ setuptools.setup(
     name="impedance_extend",
     version=__version__,
     author="impedance_extend.py developers",
-    author_email="matt.murbach@gmail.com",
-    description="A package for analyzing electrochemical impedance data",
+    author_email="prof.krishna.v@gmail.com",
+    description="A package for analyzing electrochemical impedance data "
+                "(with support for GA, PSO and least_squares or combinations "
+                "thereof)",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://impedancepy.readthedocs.io/en/latest/",
@@ -17,7 +19,7 @@ setuptools.setup(
     python_requires="~=3.10",
     install_requires=['altair>=3.0', 'matplotlib>=3.5',
                       'numpy>=1.22.4', 'scipy>=1.0',
-                      'pandas'],
+                      'pandas', 'pygad>=3.6.0', 'pyswarms>=1.3'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
